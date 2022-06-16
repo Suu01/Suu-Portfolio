134e48054a3f47ef7a53ed1b5f4c9f077583d8b8
Suu01 <ksj19990823@daum.net>
2022-06-16 13:42:21 +0900

    suu

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ window.onload = function() {
     const button = ()=> {
         const burger = document.getElementById("burger");
         const sMenu = document.getElementById("sub-menu");
+
         burger.addEventListener('click', ()=> {
             burger.classList.toggle('toggle');
             sMenu.classList.toggle('title-none');
         });
     }
-    
     button();
+
+    // 추가 -> header 메뉴 클릭 시, 서브메뉴 접히게
 }
