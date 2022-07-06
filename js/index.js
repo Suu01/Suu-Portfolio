3528770b8679195c330a677f4ce45d1bc13f1e4b
Suu01 <ksj19990823@daum.net>
2022-07-06 12:45:57 +0900

    스크롤 smooth vanilla js

--- js/index.js
+++ js/index.js
@@ -1,45 +1,116 @@
 window.onload = function() {
 
     // header 상세메뉴의 스크립트 동작 이벤트
-    const button = ()=> {
-        const burger = document.getElementById("burger");
-        const sMenu = document.getElementById("sub-menu");
+    const burger = document.getElementById("burger");
+    const sMenu = document.getElementById("sub-menu");
 
-        const sMenuBtn1 = document.querySelector(".s-menu-btn1");
-        const sMenuBtn2 = document.querySelector(".s-menu-btn2");
-        const sMenuBtn3 = document.querySelector(".s-menu-btn3");
-        const sMenuBtn4 = document.querySelector(".s-menu-btn4");
-        const sMenuBtn5 = document.querySelector(".s-menu-btn5");
+    const sMenuBtn1 = document.querySelector(".s-menu-btn1");
+    const sMenuBtn2 = document.querySelector(".s-menu-btn2");
+    const sMenuBtn3 = document.querySelector(".s-menu-btn3");
+    const sMenuBtn4 = document.querySelector(".s-menu-btn4");
+    const sMenuBtn5 = document.querySelector(".s-menu-btn5");
 
-        burger.addEventListener('click', ()=> {
-            burger.classList.toggle('toggle');
-            sMenu.classList.toggle('title-none');
+    burger.addEventListener('click', ()=> {
+        burger.classList.toggle('toggle');
+        sMenu.classList.toggle('title-none');
+    });
+    sMenuBtn1.addEventListener('click', ()=> {
+        sMenu.classList.toggle('title-none');
+        burger.className = "burger";
+        
+        var location = document.querySelector("#main-section").offsetTop;
+        window.scroll({
+            behavior: 'smooth',
+            sleft: 0,
+            top: location
         });
+    });
+    sMenuBtn2.addEventListener('click', ()=> {
+        sMenu.classList.toggle('title-none');
+        burger.className = "burger";
 
-        sMenuBtn1.addEventListener('click', ()=> {
-            sMenu.classList.toggle('title-none');
-            burger.className = "burger";
+        var location2 = document.querySelector("#about").offsetTop;
+        window.scroll({
+            behavior: 'smooth',
+            sleft: 0,
+            top: location2
         });
+    });
+    sMenuBtn3.addEventListener('click', ()=> {
+        sMenu.classList.toggle('title-none');
+        burger.className = "burger";
 
-        sMenuBtn2.addEventListener('click', ()=> {
-            sMenu.classList.toggle('title-none');
-            burger.className = "burger";
+        var location3 = document.querySelector("#skills").offsetTop;
+        window.scroll({
+            behavior: 'smooth',
+            sleft: 0,
+            top: location3
         });
+    });
+    sMenuBtn4.addEventListener('click', ()=> {
+        sMenu.classList.toggle('title-none');
+        burger.className = "burger";
 
-        sMenuBtn3.addEventListener('click', ()=> {
-            sMenu.classList.toggle('title-none');
-            burger.className = "burger";
+        var location4 = document.querySelector("#portfolio").offsetTop;
+        window.scroll({
+            behavior: 'smooth',
+            sleft: 0,
+            top: location4
         });
+    });
+    sMenuBtn5.addEventListener('click', ()=> {
+        sMenu.classList.toggle('title-none');
+        burger.className = "burger";
 
-        sMenuBtn4.addEventListener('click', ()=> {
-            sMenu.classList.toggle('title-none');
-            burger.className = "burger";
+        var location5 = document.querySelector("#contact").offsetTop;
+        window.scroll({
+            behavior: 'smooth',
+            sleft: 0,
+            top: location5
         });
+    });
 
-        sMenuBtn5.addEventListener('click', ()=> {
-            sMenu.classList.toggle('title-none');
-            burger.className = "burger";
+    
+    // 스크롤 부드럽게 이동하는 스크립트
+    document.querySelector(".home-m").addEventListener('click', function(){
+        alert("클릭되었어요");
+        var location = document.querySelector("#main-section").offsetTop;
+        window.scroll({
+            behavior: 'smooth',
+            sleft: 0,
+            top: location
         });
-    }
-    button();
+    });
+    document.querySelector(".about-m").addEventListener('click', function(){
+        var location2 = document.querySelector("#about").offsetTop;
+        window.scroll({
+            behavior: 'smooth',
+            sleft: 0,
+            top: location2
+        });
+    });
+    document.querySelector(".skills-m").addEventListener('click', function(){
+        var location3 = document.querySelector("#skills").offsetTop;
+        window.scroll({
+            behavior: 'smooth',
+            sleft: 0,
+            top: location3
+        });
+    });
+    document.querySelector(".portfolio-m").addEventListener('click', function(){
+        var location4 = document.querySelector("#portfolio").offsetTop;
+        window.scroll({
+            behavior: 'smooth',
+            sleft: 0,
+            top: location4
+        });
+    });
+    document.querySelector(".contact-m").addEventListener('click', function(){
+        var location5 = document.querySelector("#contact").offsetTop;
+        window.scroll({
+            behavior: 'smooth',
+            sleft: 0,
+            top: location5
+        });
+    });
 };
